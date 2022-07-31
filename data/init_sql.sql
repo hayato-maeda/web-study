7a3e011353026ad6f7629f23e69076a220bba51c
hayato-maeda <hayato.minto1011@gmail.com>
2022-07-31 18:10:28 +0900

    SQL-init修正

--- data/init_sql.sql
+++ data/init_sql.sql
@@ -1,1 +1,2 @@
-insert into userinfo (userid,username,email) values ('user01','管理者01','hayato.minto1011@gmail.com');
+insert into public.userinfo (userid,username,email) values ('user01','管理者01','test1@gmail.com');
+insert into public.userinfo (userid,username,email) values ('user02','管理者02','test2@gmail.com');]
