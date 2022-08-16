eced27d232155678b9fc41154ac3a000c9220e57
hayato-maeda <hayato.minto1011@gmail.com>
2022-08-16 23:14:58 +0900

    API練習

--- frontend/pages/cm/index.tsx
+++ frontend/pages/cm/index.tsx
@@ -1,13 +1,41 @@
-import { Button, Container, Grid, Typography, TextField } from "@mui/material";
+import {
+  Button,
+  Container,
+  Grid,
+  Typography,
+  TextField,
+  CircularProgress,
+  TableContainer,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { useGetUserInfoQuery } from "../../src/cm/@generated/graphql";
 import { GraphQLClient } from "graphql-request";
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosRequestConfig } from "axios";
 
 type LoginForm = {
   userid: string;
   password: string;
+};
+
+type sikuCode = {
+  prefCode: number;
+  cityCode: string;
+  cityName: string;
+  bigCityFlag: string;
+};
+
+type APIDATA = {
+  messeage: string;
+  result: Array<sikuCode>;
 };
 
 const Home: NextPage = () => {
@@ -30,21 +58,63 @@ const Home: NextPage = () => {
     where: { userid: "user01" },
   });
 
+  //------------API練習-------------------------
+  const config: AxiosRequestConfig = {
+    headers: { "X-API-KEY": "1RaI2Vm1p716f4yvXCoBCYVyHVqZJZ82aL8OKyFu" },
+  };
+
+  const { isLoading, data: apiData } = useQuery(["repoData"], () =>
+    axios.get(
+      "https://opendata.resas-portal.go.jp/api/v1/cities?prefCode=9",
+      config
+    )
+  );
+
+  if (isLoading) {
+    return <CircularProgress />;
+  }
+
+  const data2 = apiData?.data as APIDATA;
+  console.log(data2);
+
   return (
     <Container
       component="form"
-      maxWidth="xs"
+      maxWidth="lg"
       sx={{ textAlign: "center" }}
       onSubmit={handleSubmit(handleLogin)}
       noValidate
     >
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Typography component="h1" variant="h5" sx={{ mt: 10, mb: 2 }}>
+          <Typography component="h1" variant="h5" sx={{ mt: 5, mb: 1 }}>
             ホーム画面
           </Typography>
         </Grid>
       </Grid>
+      <TableContainer component={Paper}>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>都道府県コード</TableCell>
+              <TableCell>都道府県</TableCell>
+              <TableCell>市区町村コード</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data2.result.map((item, index) => (
+              <TableRow
+                key={index}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="left">{item.prefCode}</TableCell>
+                <TableCell align="left">{item.cityName}</TableCell>
+                <TableCell align="left">{item.cityCode}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
